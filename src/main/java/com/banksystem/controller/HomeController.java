66525973ec7830b63c4ff06f5d244484954c39e6
dxdNvqKP <7VDO8j7kEg>
2022-01-28 11:46:14 +0200

    created web-app, added needed files for it

---
+++ src/main/java/com/banksystem/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.banksystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/", "/home"})
+    protected String  getHome()  {
+        return "home";
+    }
+}
